72b09348403c433111f5acf59bf06339e5de57ba
Kei-K23 <keiksl2301@gmail.com>
2024-02-14 11:46:20 +0630

    integrate backend api with clerk webhook to create, update and delete user

---
+++ app/api/webhooks/clerk/route.ts
@@ -0,0 +1,127 @@
+import { Webhook } from "svix";
+import { headers } from "next/headers";
+import { WebhookEvent } from "@clerk/nextjs/server";
+
+export async function POST(req: Request) {
+  // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
+  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+
+  if (!WEBHOOK_SECRET) {
+    throw new Error(
+      "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local"
+    );
+  }
+
+  // Get the headers
+  const headerPayload = headers();
+  const svix_id = headerPayload.get("svix-id");
+  const svix_timestamp = headerPayload.get("svix-timestamp");
+  const svix_signature = headerPayload.get("svix-signature");
+
+  // If there are no headers, error out
+  if (!svix_id || !svix_timestamp || !svix_signature) {
+    return new Response("Error occured -- no svix headers", {
+      status: 400,
+    });
+  }
+
+  // Get the body
+  const payload = await req.json();
+  const body = JSON.stringify(payload);
+
+  // Create a new Svix instance with your secret.
+  const wh = new Webhook(WEBHOOK_SECRET);
+
+  let evt: WebhookEvent;
+
+  // Verify the payload with the headers
+  try {
+    evt = wh.verify(body, {
+      "svix-id": svix_id,
+      "svix-timestamp": svix_timestamp,
+      "svix-signature": svix_signature,
+    }) as WebhookEvent;
+  } catch (err) {
+    console.error("Error verifying webhook:", err);
+    return new Response("Error occured", {
+      status: 400,
+    });
+  }
+
+  const eventType = evt.type;
+
+  if (eventType === "user.created") {
+    try {
+      const res = await fetch(`http://localhost:3300/api/users`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({
+          username: payload.data.username,
+          name: payload.data.last_name
+            ? `${payload.data.first_name} ${payload.data.last_name}`
+            : payload.data.first_name,
+          email: payload.data.email_addresses[0].email_address,
+          imageUrl: payload.data.image_url,
+          externalUserId: payload.data.id,
+        }),
+        next: {
+          revalidate: 0,
+        },
+      });
+    } catch (e: any) {
+      return new Response("Something went wrong when creating user.", {
+        status: 500,
+      });
+    }
+  }
+
+  if (eventType === "user.updated") {
+    try {
+      await fetch(`http://localhost:3300/api/users/${payload.data.id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({
+          username: payload.data.username,
+          name: payload.data.last_name
+            ? `${payload.data.first_name} ${payload.data.last_name}`
+            : payload.data.first_name,
+          email: payload.data.email_addresses[0].email_address,
+          imageUrl: payload.data.image_url,
+        }),
+        next: {
+          revalidate: 0,
+        },
+      });
+    } catch (e: any) {
+      return new Response("Something went wrong when updating user.", {
+        status: 500,
+      });
+    }
+  }
+
+  if (eventType === "user.deleted") {
+    try {
+      await fetch(`http://localhost:3300/api/users/${payload.data.id}`, {
+        method: "DELETE",
+        headers: {
+          Accept: "application/json",
+        },
+        next: {
+          revalidate: 0,
+        },
+      });
+    } catch (e: any) {
+      return new Response("Something went wrong when deleting user.", {
+        status: 500,
+      });
+    }
+  }
+
+  return new Response("", { status: 200 });
+}
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
     <main>
-      <UserButton afterSignOutUrl="/" />
+      <UserButton afterSignOutUrl="/sign-in" afterSwitchSessionUrl="/sign-in" />
     </main>
   );
 }
